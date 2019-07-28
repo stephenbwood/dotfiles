9bac4c32bfaddf82b6613e9d57475f2011248a60
Stephen Wood <stephen@photovisionprints.com>
2019-07-28 09:09:25 -0700

    add termite.config

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ ln -s .vimrc ~/.vimrc
 ln -s .Xresources ~/.Xresources
 ln -s .zshrc ~/.zshrc
 ln -s .zsh_plugins.txt ~/.zsh_plugins.txt
+ln -s ~/dotfiles/termite.config ~/.config/termite/config
 
 # TODO: Install antibody
 # TODO: Install plugins from zsh_plugins
