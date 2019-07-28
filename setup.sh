b9761636a3d60a2814af01aa72e24bbbfceb1080
Stephen Wood <stephen@photovisionprints.com>
2019-07-28 08:27:38 -0700

    add TODO list

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
-cp -s .* ~/
+# Copy the dotfiles
+ln -s .vimrc ~/.vimrc
+ln -s .Xresources ~/.Xresources
+ln -s .zshrc ~/.zshrc
+ln -s .zsh_plugins.txt ~/.zsh_plugins.txt
+
+# TODO: Install antibody
+# TODO: Install plugins from zsh_plugins
+# TODO: Install vim-plug
+# TODO: Run PlugInstall
